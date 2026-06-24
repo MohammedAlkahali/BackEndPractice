c72ae1811f7632bcc3358d862f724f7164f9a48f
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-24 12:13:21 +0400

    Added the system storage - the actual storage in the memory for the lists.

--- FlightManagementSystem/Program.cs
+++ FlightManagementSystem/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace FlightManagementSystem
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem
 {
-    internal class Program
+    public class Program
     {
+
+        // System Stoarge (The actual storage in the memory for all the lists)
+        public static FlightContext context = new FlightContext
+        {
+            passengers = new List <Passenger> (),
+
+            pilots     = new List <Pilot>     (),
+
+            aircrafts  = new List <Aircraft>  (),
+
+            flights    = new List <Flight>    (),
+
+            bookings   = new List <Booking>   ()
+        };
+        // Now there are 5 empty lists that are exist in the memory under context.
+         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
+
