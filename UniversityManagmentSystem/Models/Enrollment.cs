da0506d6327eb69db46afb8449eb2e2cc57af4d7
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-30 15:22:34 +0400

    Enrollment file - Declared all the variable, data type, and its source tags

--- UniversityManagmentSystem/Models/Enrollment.cs
+++ UniversityManagmentSystem/Models/Enrollment.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace UniversityManagmentSystem.Models
 {
-    internal class Enrollment
+    public class Enrollment
     {
+        public int enrollmentId { get; set; }        // System generated 
+        public int studentId { get; set; }           // From list
+        public int courseId { get; set; }            // From list
+        public DateTime enrollmentDate { get; set; } // User input
+        public string finalGrade { get; set; }       // User input
+        public string status { get; set; }           // Default value
     }
 }
