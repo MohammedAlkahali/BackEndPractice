bd2da11ca09b2fb85598505d0b757b9533300bf0
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-30 14:54:48 +0400

    Declare all the variable, data type, and its source tags

--- UniversityManagmentSystem/Models/Student.cs
+++ UniversityManagmentSystem/Models/Student.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace UniversityManagmentSystem.Models
 {
-    internal class Student
+    public class Student
     {
+        public int studentId {  get; set; }         // System Generated 
+        public string fullName { get; set; }        // User Input
+        public string email { get; set; }           // User Input
+        public string phoneNumber { get; set; }     // User Input
+        public DateTime dateOfBirth { get; set; }   // User Input
+        public int enrollmentYear { get; set; }     // User Input 
+        public decimal gpa { get; set; }            // User Input
     }
 }
