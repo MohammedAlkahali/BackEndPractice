8a259fe131a098659371e458a39e2ab7573f0dbd
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-30 17:01:48 +0400

    Added some of the data annotations for the enrollment class

--- UniversityManagmentSystem/Models/Enrollment.cs
+++ UniversityManagmentSystem/Models/Enrollment.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversityManagmentSystem.Models
 {
     public class Enrollment
     {
-        public int enrollmentId { get; set; }        // System generated 
-        public int studentId { get; set; }           // foreign key
-        public int courseId { get; set; }            // foreign key
-        public DateTime enrollmentDate { get; set; } // User input
-        public string finalGrade { get; set; }       // User input
-        public string status { get; set; }           // Default value
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int enrollmentId { get; set; }               // System generated 
+
+        public int studentId { get; set; }                  // foreign key
+
+        public int courseId { get; set; }                   // foreign key
+
+        [Required]
+        public DateTime enrollmentDate { get; set; }        // User input
+
+        [MaxLength(2)]
+        public string? finalGrade { get; set; }             // User input
+
+        [Required]
+        [MaxLength(20)]
+        public string status { get; set; } = "In Progress"; // Default value
     }
 }
