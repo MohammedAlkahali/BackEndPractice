cd3d61100bf0077f7c0dca58a69c1903ca92551c
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-30 15:14:03 +0400

    Course file - Declared all the variable, data type, and its source tags

--- UniversityManagmentSystem/Models/Course.cs
+++ UniversityManagmentSystem/Models/Course.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace UniversityManagmentSystem.Models
 {
-    internal class Course
+    public class Course
     {
+        public int courseId { get; set; }           // System generated
+        public string courseCode { get; set; }      // User input
+        public string courseTitle { get; set; }     // User input
+        public int creditHours { get; set; }        // User input 
+        public int departmentId { get; set; }       // From list
+        public int instructorId { get; set; }       // From list
+        public string semesterOffered { get; set; } // User input 
     }
 }
