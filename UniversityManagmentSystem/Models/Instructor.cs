0dfc6ea39305a7482ac8dc5ee830623c107a5dd2
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-30 15:03:51 +0400

    Instructor file - Declared all the variable, data type, and its source tags

--- UniversityManagmentSystem/Models/Instructor.cs
+++ UniversityManagmentSystem/Models/Instructor.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace UniversityManagmentSystem.Models
 {
-    internal class Instructor
+    public class Instructor
     {
+        public int instructorId {  get; set; }      // System Generated 
+        public string fullName { get; set; }        // User input
+        public string email { get; set; }           // User input
+        public string officeNumber { get; set; }    // User input
+        public DateTime hireDate { get; set; }      // User input
+        public decimal salary { get; set; }         // User input
+        public string academicTitle { get; set; }   // User input
     }
 }
