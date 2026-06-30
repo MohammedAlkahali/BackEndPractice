390ee8fab20302934bc53dc41091bd3fc7e02803
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-30 16:41:25 +0400

    Added some of the data annotations for the Department class

--- UniversityManagmentSystem/Models/Department.cs
+++ UniversityManagmentSystem/Models/Department.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace UniversityManagmentSystem.Models
 {
+    [Index(nameof(departmentName), IsUnique = true)]
     public class Department
     {
-
+        [Key]
+        [Required]
         public int departmentId { get; set; }       // System generated 
+
+        [Required]
+        [MaxLength(100)]
         public string departmentName { get; set; }  // User input
-        public string building {  get; set; }       // User input
+
+        [MaxLength(50)]
+        public string? building {  get; set; }       // User input
+
+        [Required]
         public decimal budget { get; set; }         // User input
+
+
         public int headInstructorId { get; set; }   // foreign key
 
     }
