1bef14876783399709cf5dea9e9dda13dc04167b
MohammedAlkahali <mohammedharib21@gmail.com>
2026-06-30 16:58:13 +0400

    Added some of the data annotations for the course class

--- UniversityManagmentSystem/Models/Course.cs
+++ UniversityManagmentSystem/Models/Course.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversityManagmentSystem.Models
 {
+    [Index(nameof(courseCode), IsUnique = true)]
     public class Course
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         public int courseId { get; set; }           // System generated
+
+        [Required]
+        [MaxLength(10)]
         public string courseCode { get; set; }      // User input
+
+        [Required]
+        [MaxLength(150)]
         public string courseTitle { get; set; }     // User input
+
+        [Required]
+        [Range(1, 6)]
         public int creditHours { get; set; }        // User input 
+
         public int departmentId { get; set; }       // foreign key
-        public int instructorId { get; set; }       // foreign key
+
+        public int? instructorId { get; set; }       // foreign key
+
+        [Required]
+        [MaxLength(20)]
         public string semesterOffered { get; set; } // User input 
     }
 }
