5bb33412f8824515e793ec19fc39dc178ee6f7f6
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-01 15:19:06 +0400

    Order class - Added the names, data types, and its source tags

--- E-CommerceManagmentSystem/Models/Order.cs
+++ E-CommerceManagmentSystem/Models/Order.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace E_CommerceManagmentSystem.Models
 {
-    internal class Order
+    public class Order
     {
+        public int      orderId         { get; set; }   // System generated 
+        public int      userId          { get; set; }   // Foreign key
+        public DateTime OrderDate       { get; set; }   // User input
+        public decimal  totalAmount     { get; set; }   // User input
+        public string   status          { get; set; }   // User input
+        public string   shippingAddress { get; set; }   // User input
+        public string   paymentMethod   { get; set; }   // User input
     }
 }
