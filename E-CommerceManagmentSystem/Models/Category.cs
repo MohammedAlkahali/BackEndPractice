ae0aad750e35b659e380312316b8b5a695663f6f
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-01 16:00:07 +0400

    Category class - Added the data annotation.

--- E-CommerceManagmentSystem/Models/Category.cs
+++ E-CommerceManagmentSystem/Models/Category.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_CommerceManagmentSystem.Models
 {
+    [Index(nameof(categoryName), IsUnique = true)]
     public class Category
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]// Data annotation
         public int    categoryId    { get; set; }   // System generated  
+
+        [Required, MaxLength(100)]                                          // Data annotation
         public string categoryName  { get; set; }   // User input
-        public string description   { get; set; }   // User input
-        public string imageUrl      { get; set; }   // User input
+
+        [MaxLength(500)]                                                    // Data annotation
+        public string? description   { get; set; }   // User input
+
+        [MaxLength(300)]                                                    // Data annotation
+        public string? imageUrl      { get; set; }   // User input
     }
 }
