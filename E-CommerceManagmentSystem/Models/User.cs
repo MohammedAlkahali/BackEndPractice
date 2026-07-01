be39bfcd7e8f06f3cdde683dcb00813a54e2c167
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-01 15:02:09 +0400

    User class - Added the names, data types, and its source tags

--- E-CommerceManagmentSystem/Models/User.cs
+++ E-CommerceManagmentSystem/Models/User.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace E_CommerceManagmentSystem.Models
 {
-    internal class User
+    public class User
     {
+        public int      userId           { get; set; }           // System generated
+        public string   username         { get; set; }           // User input
+        public string   email            { get; set; }           // User input
+        public string   passwordHash     { get; set; }           // User input
+        public string   fullName         { get; set; }           // User input
+        public string   phoneNumber      { get; set; }           // User input
+        public string   address          { get; set; }           // User input
+        public DateTime registrationDate { get; set; }           // User input
+        public bool     isActive         { get; set; }           // Default value
     }
 }
