01bda115d32808c8f8217d4c44eba11594ea03f6
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-01 15:05:37 +0400

    Category class - Added the names, data types, and its source tags

--- E-CommerceManagmentSystem/Models/Category.cs
+++ E-CommerceManagmentSystem/Models/Category.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace E_CommerceManagmentSystem.Models
 {
-    internal class Category
+    public class Category
     {
+        public int    categoryId    { get; set; }   // System generated  
+        public string categoryName  { get; set; }   // User input
+        public string description   { get; set; }   // User input
+        public string imageUrl      { get; set; }   // User input
     }
 }
