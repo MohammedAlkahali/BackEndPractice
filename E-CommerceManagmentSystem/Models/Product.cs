b239ada0a3a4a693c79d96b1291b35442d544d59
MohammedAlkahali <mohammedharib21@gmail.com>
2026-07-01 15:12:46 +0400

    Product class - Added the names, data types, and its source tags

--- E-CommerceManagmentSystem/Models/Product.cs
+++ E-CommerceManagmentSystem/Models/Product.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace E_CommerceManagmentSystem.Models
 {
-    internal class Product
+    public class Product
     {
+        public int      productId     { get; set; } // System generated 
+        public string   productName   { get; set; } // User input 
+        public string   description   { get; set; } // User input
+        public decimal  price         { get; set; } // User input
+        public int      stockQuantity { get; set; } // User input
+        public string   imageUrl      { get; set; } // User input 
+        public int      categoryId    { get; set; } // User input 
+        public DateTime createdAt     { get; set; } // User input
+        public bool     isAvailable   { get; set; } // Default value
     }
 }
